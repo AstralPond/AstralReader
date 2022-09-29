57177cf75637e13a1c76836f52d9237476105878
John Dinh <johnduydinh@gmail.com>
2022-09-29 15:20:48 -0400

    create watcher to redirect /login & /dashboard

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,4 +6,23 @@
 
 <script setup lang="ts">
 import "./scss/main.module.scss";
+import { useStore } from "@/stores/main";
+import { useRouter } from "vue-router";
+
+const store = useStore();
+const router = useRouter();
+
+// Runs everytime state is changed
+// Redirects page to /dashboard or /login based on isAuthenticated
+store.$subscribe(
+  (_mutation, state) => {
+    const { isAuthenticated } = state.auth;
+    if (isAuthenticated) {
+      router.replace({ name: "dashboard" });
+    } else {
+      router.replace({ name: "login" });
+    }
+  },
+  { immediate: true }
+);
 </script>
