22beb684f5b146495b35eb556d4594751a15d191
John Dinh <johnduydinh@gmail.com>
2022-09-29 15:18:45 -0400

    create auth composable

---
+++ client/src/use/auth.ts
@@ -0,0 +1,61 @@
+import { State, useStore } from "@/stores/main";
+import { Store, storeToRefs } from "pinia";
+import { Router, useRouter } from "vue-router";
+
+export default function useAuth(s: Store<"main", State>, r: Router) {
+  const store = s || useStore();
+  const router = r || useRouter();
+  const { auth } = storeToRefs(store);
+  loadCookie(store);
+
+  return { auth, login: login(store, router) };
+}
+
+interface LoginBody {
+  email: string;
+  password: string;
+}
+
+function login(store: Store, router: Router) {
+  return async (body: LoginBody) => {
+    try {
+      const response = await fetch("http://localhost:3000/gatekeeper/login", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const data = await response.json();
+
+      if (data.email) {
+        store.$patch((state: State) => {
+          state.auth.user = data;
+        });
+
+        router.push({ name: "dashboard" });
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      console.log("done");
+    }
+  };
+}
+
+async function loadCookie(store: Store) {
+  // Update auth store
+  const response = await fetch("http://localhost:3000/gatekeeper", {
+    credentials: "include",
+  });
+
+  const data = await response.json();
+
+  if (data.email) {
+    store.$patch((state: State) => {
+      state.auth.user = data;
+    });
+  }
+}
