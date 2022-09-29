7ce5fe5a26b69282a752c162dfd0e3c31d250df9
John Dinh <johnduydinh@gmail.com>
2022-09-29 15:22:05 -0400

    remove unused property from auth store

--- client/src/stores/main.ts
+++ client/src/stores/main.ts
@@ -7,18 +7,16 @@ interface User {
 
 interface Auth {
   user: null | User;
-  email: string;
   isAuthenticated: boolean;
 }
 
-interface State {
+export interface State {
   auth: Auth;
 }
 
 export const useStore = defineStore("main", (): State => {
   const auth: Auth = reactive({
     user: null,
-    email: "",
     isAuthenticated: computed(() => {
       return !!auth?.user?.email;
     }),
