09089bd8e6c29c1db3afc734427f30ee40c94dde
John Dinh <johnduydinh@gmail.com>
2022-09-29 15:21:23 -0400

    rename tmux window

--- client/tmux-start.sh
+++ client/tmux-start.sh
@@ -5,7 +5,7 @@ session="client"
 tmux new-session -d -s $session
 
 window=0
-tmux rename-window -t $session:$window 'ui'
+tmux rename-window -t $session:$window 'term'
 tmux send-keys -t $session:$window 'npm run serve' C-m
 
 window=1
