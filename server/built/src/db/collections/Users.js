fb9dab212e7b94f239c673b2b4f96faa1ba7555b
John Dinh <johnduydinh@gmail.com>
2022-10-06 15:34:21 -0400

    remove apollo-server and the 'built' folder

--- server/built/src/db/collections/Users.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var db_1 = require("db");
-exports.default = db_1.db.collection("users");
-//# sourceMappingURL=Users.js.map
--- server/built/src/db/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-var mongodb_1 = require("mongodb");
-var uri = "mongodb://john:cody4454@11.11.11.66:27017";
-var client = new mongodb_1.MongoClient(uri);
-console.log("================i run==============");
-exports.db = client.db("astro");
-//# sourceMappingURL=index.js.map
